16c834168f1576b0c28aac1795224c0d42424466
miaout11 <miaout.11@gmail.com>
2022-10-27 12:45:21 +0800

    add handlebars

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
-
+// db
 require('./config/mongoose')
 
+// set template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+// express body-parser
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-  res.send('This is a express project')
+  res.render('index')
 })
 
 app.listen(port, () => {
